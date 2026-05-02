8bc448c4a03430d05da407de16e96d528d62bbb3
Aksel Lindberg <aksel.lindberg@lintech.no>
2026-05-02 20:30:27 +0200

    Initial Commit

---
+++ SnorryTests/SnorryTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnorryTests.swift
+//  SnorryTests
+//
+//  Created by Aksel Lindberg on 02/05/2026.
+//
+
+import Testing
+@testable import Snorry
+
+struct SnorryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
